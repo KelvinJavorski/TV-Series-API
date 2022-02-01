11cf4e3dd69db69a7c76212cf83787b480b65f9a
Kelvin Javorski <kelvin.javorskisoares@gmail.com>
2022-01-31 22:39:12 -0300

    Add properties to the json object

--- IOS Challenge - TV API/Model/Show.swift
+++ IOS Challenge - TV API/Model/Show.swift
@@ -5,5 +5,32 @@ struct Show: Codable {
     let id: Int
     let url: String
     let name: String
+    let image: Image?
+    let genres: [String]
+    let runtime, avarageRuntime: Int?
+    let schedule: Schedule
+    let summary: String
+    let _embedded: Embedded?
+}
 
+struct Image: Codable {
+    let medium, original: String
+}
+
+struct Schedule: Codable {
+    let time: String
+    let days: [String]
+}
+
+struct Embedded: Codable {
+    let episodes: [Episode]?
+}
+
+struct Episode: Codable {
+    let id: Int
+    let url: String
+    let name: String
+    let season: Int
+    let number: Int
+    let image: Image?
 }
