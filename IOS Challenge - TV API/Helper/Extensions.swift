ddd0e51b1961580db00d1adea79ead40dd1a06a3
Kelvin Javorski <kelvin.javorskisoares@gmail.com>
2022-01-31 22:38:56 -0300

    Added extension to load UIImage from URL

--- IOS Challenge - TV API/Helper/Extensions.swift
+++ IOS Challenge - TV API/Helper/Extensions.swift
@@ -5,4 +5,23 @@
 //  Created by Kelvin Javorski Soares on 31/01/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func load(url: String) {
+        guard let url = URL(string: url) else { return }
+        self.load(url: url)
+    }
+
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
